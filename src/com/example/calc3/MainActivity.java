18a966e2a34ceb0d8816650637ebaed420222d64
Gary Tse <garytse89@gmail.com>
2013-02-26 14:09:10 -0800

    Wrote a lot of comments!

--- src/com/example/calc3/MainActivity.java
+++ src/com/example/calc3/MainActivity.java
@@ -11,39 +11,62 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-// the first step was to implement onclicklistener
+
+// This is a primitive calculator application with some bugs.
+// I implemented an original algorithm without any research or help, so the way the calculations are done and stored are perhaps very "primitive"
+// For my comments: [...] implies a variable
+// The word 'operator' means +, -, x, /, etc.
+
+// The code is perhaps really confusing, so let me try to summarize as quickly as possible what happens when I enter:
+// 8+2x5
+// A string array called data[] becomes [ "8" | "+" | "2" | "x" | "5" ]
+// operatorFlag is triggered because there is a multiplication or division (i.e. a priority operator)
+// The array index of the multiplication operator is also returned, in this case "x" is at index = 3
+// 2x5 is calculated first and stored into a double array called resultInter[]
+// resultInter becomes [10] and is instantly transferred back into data[]
+// data[] gets its multiplication operator and the original multiplying values replaced with 0's and the product, becoming [ "8" | "+" | "0"  | "10" | "0" ]
+// Now we just use a for-loop to loop through the whole data[] index and either add or subtract;
+// store this math into resultTemp (a double number that gets incremented by added or subtracted numbers) then convert it to string so the user can see the result.
+
+// You can think of it as:
+// if there is only addition or subtraction, then only data[] is looped through and the numbers incremented or decremented based on 
+// a "+" or "-" sign, which is what I call a lower-level sequence (the code is simple and short)
+// If there is multiplication or division (and a half-implemented bracket and exponent), then we evaluate those products first,
+// throwing numbers back and forth between two arrays, until we end up replacing all the mul/div operator signs and end up with a casual expression.
+// Then evaluate as a casual, lower-level addition/subtraction sequence afterwards.
+
 public class MainActivity extends Activity implements OnClickListener {
-// second step, defining buttons
+
+	// Initialize the visual layout components: textview for displaying the numbers and operators, and the buttons (quite a few)
 	TextView textResult, specialView;
 	Button b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bAdd, bSub, bMul, bDiv, bC, bBrackets, bBack, bEqual, bDecimal, bPosNeg, bExponent;
+	
+	// Here comes a bunch of variables 
 	static String data[] = new String[100];
 	static int index = 0;
 	
 	static double resultNum = 0;
-	
-	static double resultTemp = 0; // modify this into an array later
+		static double resultTemp = 0; 
 	static int bracketCount = 0;
 	static String results = "";
 	static int decimalFlag = 0;
 	static double decimalCounter = 10;
 	static int operatorAllowed = 0;
-	
-	static int operatorFlag = 0; 
+		static int operatorFlag = 0; 
 	static int addFlag = 1; // by default, we do addition. 
 	static int negFlag = 0;
 	static int operatorPosition[] = new int[10];
 	static double resultInter[] = new double[10];
-	
-	
-	static String carrier = "";
+		
 	static int temp = 0;
 	
 		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// "Initialize" the layout
 		setContentView(R.layout.activity_main);
-		// third step: linking all those damn buttons
+		// Link all the buttons
 		b0 = (Button) findViewById(R.id.b0);
 		b1 = (Button) findViewById(R.id.b1);
 		b2 = (Button) findViewById(R.id.b2);
@@ -68,7 +91,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		// the textview
 		textResult = (TextView) findViewById(R.id.textResult);
 		specialView = (TextView) findViewById(R.id.specialView);
-		// fourth step: all the onclick listeners
+		// Set all the buttons to eventually respond to clicks/presses
 		b0.setOnClickListener(this); 
 		b1.setOnClickListener(this); 
 		b2.setOnClickListener(this); 
@@ -92,7 +115,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		bExponent.setOnClickListener(this);
 	}
 	
-	// i dont care about menus at the moment
+	// No menus at the moment
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -105,14 +128,36 @@ public class MainActivity extends Activity implements OnClickListener {
 		
 	}
 	
+	// The code where everything happens
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		
-		// switch case code for each button
-		// b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bAdd, bSub, bMul, bDiv, bC, bBrackets, bBack, bEqual, bDecimal, bPosNeg
+		// There is a switch-case code for each button
 		switch (v.getId()) {
-			case R.id.b0:
+		
+			// Explanation for the code for number buttons 0-9:
+			// The variable [results] is a STRING ARRAY. It is added to every time I press a button for a number or operator
+		    // so that the user can see what he or she is inputting
+		
+			// [operatorAllowed] is a flag variable to prevent the user from pressing two operators in a row.
+		    // as such, a valid mathematical expression can only contain a number, then an operator, then a number, and so forth.
+		
+			// [decimalFlag] is another flag variable for when I have inputted a decimal "." sign.
+			// Logically, I cannot have multiple decimals in a single number, like 1.2.3 (not valid), so I have flags to prevent that
+			// [decimalCounter] divides the inputted number accordingly based on how far behind the decimal point the number is
+			// So for example, I enter the 3 in 1.23, by then decimalCounter would equal 100, so 3/100 = 0.03 added onto 1.20
+		
+			// [resultNum] is where all the math side of the application is done and this isn't shown to the user.
+			// [resultNum] is reset to 0 everytime the user enters "C" (clear) or the equal sign more than once (this is a bug I've been to lazy to fit).
+			// [resultNum] is multiplied by 10 and the number button pressed is incremented on top.
+			
+			// So for example, if I enter "8" and then "2" to try to get EIGHTY-TWO, the math that happens is:
+			// resultNum = resultNum * 10 ===> 0
+			// resultNum += 8 ===> 0+8=8
+			// resultNum = resultNum * 10 ===> 8*10 = 80
+			// resultNum += 2 ===> 80+2 = 82
+		case R.id.b0:
 				// Put into string array for BOTH display and later math conversions
 				results += "0";
 				operatorAllowed = 1;
@@ -272,6 +317,23 @@ public class MainActivity extends Activity implements OnClickListener {
 					decimalFlag = 0;
 				}
 				break;
+				
+				
+			// This is the OPERATORS section
+			// What happens first is the code needs to know if an operator input is allowed or not. This is to prevent invalid expressions
+			// from being entered.
+			
+			// The string representation showed to the user gets the operator sign tacked on (no-brainer).
+			// Then here's the tougher part:
+			// The [resultNum] previously incremented from entering the number will be stored, as a STRING, to a string array called data[]
+			// The reason for this you will see soon.
+				
+			// For example's sake, let's just say you just entered 82+ in the calculator
+			// [resultNum] would equal 82 in double format
+			// The moment you press the "+" button, what happens is 82 becomes "82" (string) and is stored into the data array
+			// The array index is incremented and the string "+" is put in
+			// So data[] is now ["82"|"+"]
+			
 			case R.id.bAdd:
 				if( operatorAllowed == 1){
 					// Display operator sign on display
@@ -315,6 +377,9 @@ public class MainActivity extends Activity implements OnClickListener {
 					specialView.setText("");
 				}
 				break;
+			// The purpose for the operator flag is because MULTIPLICATION and DIVISION have a higher priority over add/subtract
+			// Recall "BEDMAS"
+			// So there needs to be a flag variable for that when all the numbers are processed later
 			case R.id.bMul: 
 				if( operatorAllowed == 1){
 					// Display operator sign on display
@@ -413,6 +478,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				}
 				
 				break;
+				
 			case R.id.bBack:
 				results = results.substring(0, results.length()-1);
 				// need to delete numbers on screen, but not all numbers in string array
@@ -442,6 +508,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				specialView.setText("(-)");
 				negFlag = 1;
 				break;
+				
 			case R.id.bDecimal: 
 				if( decimalFlag == 0){
 					results = results + ".";
@@ -449,9 +516,7 @@ public class MainActivity extends Activity implements OnClickListener {
 					operatorAllowed = 0;
 				}
 				break;
-			
-				
-				
+							
 			case R.id.bExponent:
 				if( operatorAllowed == 1){
 					// Display operator sign on display
@@ -477,8 +542,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				break;
 			
 			case R.id.bEqual:
-				
-						
+									
 				// Put accumulated number, converted into string format, into array space (IF ANY)
 				if (negFlag == 1)
 					resultNum = resultNum*(-1);
@@ -507,9 +571,8 @@ public class MainActivity extends Activity implements OnClickListener {
 							
 							// explanation of confusing code:
 							// I'm either multiplying or dividing the numbers ADJACENT to the mul or div operator
-							// like the chinese drinking game BING BANG WAH
-							
-							// I used an array to store the intermediate products of mul/div because there could be a lot of em... :(
+														
+							// I used an array to store the intermediate products of mul/div because there could be a lot of them
 			
 							System.out.println(temp-1);
 							if(data[temp-1] != "" || data[temp+1] != ""){
